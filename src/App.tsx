d869dd67e4f7f8e2e740d9fa34f104a5b16694dc
SKitttz <skittz.dev@gmail.com>
2024-10-20 14:19:52 -0300

    refact(App): add provider context router

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,18 @@ import { GlobalStyle } from '@styles/global';
 import { defaultTheme } from '@styles/themes/default';
 import './App.css';
 import { RouteConfig } from './routes/config';
+import { BrowserRouter } from 'react-router-dom';
+import { DefaultLayout } from './layout';
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <RouteConfig />
+      <BrowserRouter>
+        <DefaultLayout>
+          <RouteConfig />
+        </DefaultLayout>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
