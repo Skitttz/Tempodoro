289a408950de1e730bed10a931b82bfe0d43de5a
SKitttz <skittz.dev@gmail.com>
2024-11-08 23:49:08 -0300

    refact(Header): add Header component with clear history button and styles

--- src/_components/Header/index.tsx
+++ src/_components/Header/index.tsx
@@ -1,3 +1,41 @@
+import { NavLink } from 'react-router-dom';
+import { HeaderContainer } from './style';
+import Logo from '@assets/logo.svg';
+import { RoutesEnum } from 'src/routes/routes';
+import { IconStack2, IconClock, IconScriptX } from '@tabler/icons-react';
+import { deleteLocalStorage } from '@utils/deleteLocalStorage';
+import {
+  MessagesEnum,
+  TempodoroStorageKeysEnum,
+} from '@constants/generalConstants';
+
 export function Header() {
-  return <main></main>;
+  const handleDeleteStorage = (): void => {
+    const confirmDelete = window.confirm(MessagesEnum.CONFIRM_DELETE_HISTORY);
+    if (confirmDelete) {
+      const datalist = document.getElementById('task-suggestions');
+      if (datalist) {
+        datalist.innerHTML = '';
+      }
+      deleteLocalStorage(TempodoroStorageKeysEnum.SESSION_STATE);
+    }
+  };
+  return (
+    <HeaderContainer>
+      <NavLink to={RoutesEnum.Home} title="Home">
+        <img src={Logo} alt="Logo do Tempodoro" />
+      </NavLink>
+      <nav>
+        <NavLink to={RoutesEnum.Home} title="Timer">
+          <IconClock size={32} />
+        </NavLink>
+        <NavLink to={RoutesEnum.History} title="Historico">
+          <IconStack2 size={32} />
+        </NavLink>
+        <a onClick={handleDeleteStorage}>
+          <IconScriptX size={32} />
+        </a>
+      </nav>
+    </HeaderContainer>
+  );
 }
